6fcf21904235090f46ae4f62cf139eb7d0a3573a
Ritika Garg <ritika.garg.15cse@bml.edu.in>
2021-09-23 15:56:58 +0530

    constant added

--- src/screens/styles.js
+++ src/screens/styles.js
@@ -1,27 +1,26 @@
 import { StyleSheet } from 'react-native';
+import Constants from '../constants';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#444',
     flex: 1,
     alignItems: 'center',
-    // justifyContent: 'center'
   },
   titleText: {
     fontSize: 40,
     color: 'white',
   },
   boardContainer: {
-    marginTop: 50,
+    marginTop: Constants.BaseStyle.DEVICE_HEIGHT / 100 * 5,
     backgroundColor: 'skyblue',
     borderRadius: 5,
-    padding: 20,
-    alignItems: 'center'
+    padding: Constants.BaseStyle.DEVICE_WIDTH / 100 * 6,
   },
   squareContainer: {
     backgroundColor: "#444",
-    height: 100,
-    width: 100,
+    height: Constants.BaseStyle.DEVICE_HEIGHT / 100 * 12,
+    width: Constants.BaseStyle.DEVICE_HEIGHT / 100 * 12,
     alignItems: 'center',
     justifyContent: "center",
     borderColor: "#999",
